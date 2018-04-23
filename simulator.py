a6a1099fc433bc4507c5e2450fe80817a9025ca3
fheliciaselas <32631762+fheliciaselas@users.noreply.github.com>
2018-04-24 01:30:52 +0800

    Round robin update

--- simulator.py
+++ simulator.py
@@ -1,36 +1,33 @@
-'''
-CS5250 Assignment 4, Scheduling policies simulator
-Sample skeleton program
-Author: Minh Ho
-Input file:
-    input.txt
-Output files:
-    FCFS.txt
-    RR.txt
-    SRTF.txt
-    SJF.txt
 
-Apr 10th Revision 1:
-    Update FCFS implementation, fixed the bug when there are idle time slices between processes
-    Thanks Huang Lung-Chen for pointing out
-Revision 2:
-    Change requirement for future_prediction SRTF => future_prediction shortest job first(SJF), the simpler non-preemptive version.
-    Let initial guess = 5 time units.
-    Thanks Lee Wei Ping for trying and pointing out the difficulty & ambiguity with future_prediction SRTF.
-'''
 import sys
+import copy
 
 input_file = 'input.txt'
 
+buffer_time = 45
+
 class Process:
-    last_scheduled_time = 0
+
     def __init__(self, id, arrive_time, burst_time):
         self.id = id
         self.arrive_time = arrive_time
         self.burst_time = burst_time
+        self.last_scheduled_time = arrive_time
     #for printing purpose
     def __repr__(self):
         return ('[id %d : arrive_time %d,  burst_time %d]'%(self.id, self.arrive_time, self.burst_time))
+
+    
+
+def append_to_schedule(schedule,current_time, process_id):
+   
+    if len(schedule) > 0:
+        last_schedule = schedule[n-1]
+        previous_process_id = last_schedule[1]
+        if process_id != previous_process_id:
+            schedule.append((current_time, process_id))
+    else:
+        schedule.append((current_time, process_id))
 
 def FCFS_scheduling(process_list):
     #store the (switching time, proccess_id) pair
@@ -49,14 +46,56 @@ def FCFS_scheduling(process_list):
 #Input: process_list, time_quantum (Positive Integer)
 #Output_1 : Schedule list contains pairs of (time_stamp, proccess_id) indicating the time switching to that proccess_id
 #Output_2 : Average Waiting Time
-def RR_scheduling(process_list, time_quantum ):
-    return (["to be completed, scheduling process_list on round robin policy with time_quantum"], 0.0)
 
-def SRTF_scheduling(process_list):
-    return (["to be completed, scheduling process_list on SRTF, using process.burst_time to calculate the remaining time of the current process "], 0.0)
+def RR_scheduling(process_list, time_quantum):
+    schedule = []
+    current_time = 0
+    waiting_time = 0
+    Q_processing = []
+    Q_unprocessed = copy.deepcopy(process_list)
 
-def SJF_scheduling(process_list, alpha):
-    return (["to be completed, scheduling SJF without using information from process.burst_time"],0.0)
+    n = len(process_list)
+    max_time = process_list[n-1].arrive_time + process_list[n-1].burst_time + buffer_time  #max time for simulation
+    
+    while current_time < max_time:
+        while len(Q_unprocessed) > 0:
+            todo = Q_unprocessed[0]
+            if todo.arrive_time <= current_time+time_quantum:
+                Q_processing.append(todo) # add to process list
+                Q_unprocessed.pop(0) # remove from unprocessed list
+            else:
+                break
+
+        if len(Q_processing) > 0:
+            current_process = Q_processing[0]
+            
+            if current_process.arrive_time <= current_time:
+                Q_processing.pop(0)
+                
+                if current_time < current_process.last_scheduled_time:
+                    current_time = current_process.last_scheduled_time #update current time
+                
+                waiting_time = waiting_time + (current_time - current_process.last_scheduled_time); #add waiting time
+                append_to_schedule(schedule, current_time, current_process.id)  # processing
+                
+                if current_process.burst_time > time_quantum:
+                    current_time += time_quantum
+                    current_process.burst_time -= time_quantum
+                    current_process.last_scheduled_time = current_time
+                    Q_processing.append(current_process)
+                else:
+                    current_time += current_process.burst_time
+                    current_process.burst_time = 0
+                    current_process.last_scheduled_time = current_time
+            else:
+                current_time += 1
+        else:
+            current_time += 1
+
+    average_waiting_time = waiting_time / float(len(process_list))
+    
+    return schedule, average_waiting_time
+
 
 
 def read_input():
@@ -87,12 +126,7 @@ def main(argv):
     print ("simulating RR ----")
     RR_schedule, RR_avg_waiting_time =  RR_scheduling(process_list,time_quantum = 2)
     write_output('RR.txt', RR_schedule, RR_avg_waiting_time )
-    print ("simulating SRTF ----")
-    SRTF_schedule, SRTF_avg_waiting_time =  SRTF_scheduling(process_list)
-    write_output('SRTF.txt', SRTF_schedule, SRTF_avg_waiting_time )
-    print ("simulating SJF ----")
-    SJF_schedule, SJF_avg_waiting_time =  SJF_scheduling(process_list, alpha = 0.5)
-    write_output('SJF.txt', SJF_schedule, SJF_avg_waiting_time )
+    
 
 if __name__ == '__main__':
     main(sys.argv[1:])
